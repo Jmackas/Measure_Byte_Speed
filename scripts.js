ce19633899caa9d6b1f902f7ade3edd90b4490bc
James Mackay <jamesrossmackay@gmail.com>
2018-10-18 10:33:56 +0800

    Extra field added

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 function algorithm() {
     // Variable declarations
     var byteInput;
+    var transferSize;
     var byteOutput;
 
     // Retrieve user input
     byteInput = document.getElementById("byte_input").value;
+    transferSize = document.getElementById("transfer_size").value;
 
     // Algorithm
     byteOutput = byteInput;
